ef7f299b7d4c78a0ea574175da354976d015a1c0
Domingo Cabeza <domingoacd1@gmail.com>
2020-08-12 18:57:42 -0400

    Updates

---
+++ precache-manifest.82424387b3e528219e7e5bc42d41664b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f72bdad9ad7626b5e891fc7da6e4f0cd",
+    "url": "/react-pokedex/index.html"
+  },
+  {
+    "revision": "5a28c1abfd244eef0cd6",
+    "url": "/react-pokedex/static/css/main.0f02a4a1.chunk.css"
+  },
+  {
+    "revision": "af2f45ee6c9cd0204fe1",
+    "url": "/react-pokedex/static/js/2.825c1356.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-pokedex/static/js/2.825c1356.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a28c1abfd244eef0cd6",
+    "url": "/react-pokedex/static/js/main.fc84bc8b.chunk.js"
+  },
+  {
+    "revision": "8d0dc51f96d551a6d0f3",
+    "url": "/react-pokedex/static/js/runtime-main.f27a760f.js"
+  }
+]);
