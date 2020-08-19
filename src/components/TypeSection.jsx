d945b5104dcfb5a34661f27f33685bc535fa51ce
Domingo Cabeza <domingoacd1@gmail.com>
2020-08-19 17:42:19 -0400

    Added styles to pokemons type page

--- src/components/TypeSection.jsx
+++ src/components/TypeSection.jsx
@@ -1,8 +1,39 @@
 import React, {useEffect, useState} from 'react';
 import { useLocation, Link } from 'react-router-dom';
+import styled from 'styled-components';
 import api from '../api-fetcher';
 import Pokemon from './Pokemon';
 
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Title = styled.h1`
+  position: relative;
+  color: ${props => props.theme[props.type]};
+  text-transform: capitalize;
+  font-size: 3rem;
+  text-align: center;
+  line-height: 1.7rem;
+`;
+const SubTitle = styled.span`
+  color: ${props => props.theme.mainFontColor};
+  font-size: 1rem;
+  font-weight: 400;
+`;
+
+const TypeImage = styled.div`
+  position: fixed;
+  right: -5rem;
+  width: 20rem;
+  height: 20rem;
+  opacity: 0.5;
+  mask: ${props => `url("/images/${props.type}.svg")`};
+  mask-size: contain;
+  background: ${props => props.theme[props.type]};
+`
 const TypeSection = () => {
   const typeToFetch = useLocation().pathname.split('/')[2];
   const [pokemons, setPokemons] = useState([]);
@@ -57,7 +88,15 @@ const TypeSection = () => {
   }
 
   return (
-    <div>{insertPokemons()}</div>
+    <MainContainer>
+      <TypeImage type={typeToFetch}/>
+      <Title type={typeToFetch}>
+        {typeToFetch}
+        <br />
+        <SubTitle>Pokemons</SubTitle>
+      </Title>
+      {insertPokemons()}
+    </MainContainer>
   )
 }
 
